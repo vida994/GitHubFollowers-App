fc55dca9db585b3fe14458865add154b192169b0
Antonio Vidakovic <avidakovic14@gmail.com>
2021-02-16 18:30:10 +0100

    Project set up: create main tabbarVC and two navigation VC with their own Viewcontroller

---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Antonio Vidakovic on 15.02.2021..
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+
+    }
+   
+
+}
