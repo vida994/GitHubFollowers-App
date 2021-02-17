4effdf22cb77f33adaaf4625304a8c57d6e4df73
Antonio Vidakovic <avidakovic14@gmail.com>
2021-02-17 17:57:29 +0100

    Create two objects: Follower and user. Create NetworkManager.

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Antonio Vidakovic on 17.02.2021..
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ GitHubFollowers/ViewControllers/FollowerListVC.swift
@@ -0,0 +1,34 @@
+//
+//  FollowerListVC.swift
+//  GitHubFollowers
+//
+//  Created by Antonio Vidakovic on 16.02.2021..
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+    
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (result, error) in
+            guard let followers = result else {
+                self.presentGFAlertONMainThread(title: "Test", message: error!, buttonTitle: "Ok")
+                return
+            }
+            print("count= \(followers.count)")
+            print(followers)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+
+}
